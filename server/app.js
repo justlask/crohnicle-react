99ebb30bb34ece439879b2e9bc22063c77c3c29e
Lauren Laskerr <llask@me.com>
2019-11-09 12:08:52 -0500

    lets go, react + better styling

--- server/app.js
+++ server/app.js
@@ -4,22 +4,18 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors')
 
-const bcrypt       = require('bcryptjs');
 
-const flash        = require('connect-flash');
-const session    = require("express-session");
-const MongoStore = require("connect-mongo")(session);
 
-const passport     = require('passport');
-const LocalStrategy     = require('passport-local').Strategy
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session       = require('express-session');
+const passport      = require('passport');
 
-const User          = require('./models/Users')
+
+require('./config/passport');
 
 mongoose
   .connect(process.env.MONGO_URI, {useNewUrlParser: true})
@@ -41,6 +37,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -48,117 +46,45 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(express.static(path.join(__dirname, '../client/build')))
 
-
-// default value for title local
-app.locals.title = 'Crohnicle';
-
+// ADD SESSION SETTINGS HERE:
 app.use(session({
-  secret: "our-passport-local-strategy-app",
+  secret:"some secret goes here",
   resave: true,
   saveUninitialized: true
 }));
 
-
-
-passport.serializeUser((user, cb) => {
-  cb(null, user._id);
-});
-
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-
-    return next(null, user);
-  });
-}));
-
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GID,
-//       clientSecret: process.env.GSECRET,
-//       callbackURL: "/auth/google/callback"
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//       // to see the structure of the data in received response:
-//       console.log("Google account details:", profile);
-
-//       User.findOne({ googleID: profile.id })
-//         .then(user => {
-//           if (user) {
-//             done(null, user);
-//             return;
-//           }
-
-//           User.create({ 
-//             googleID: profile.id,
-//             username: profile._json.name,
-//             email: profile._json.email,
-//             isAdmin: false
-//            })
-//             .then(newUser => {
-//               done(null, newUser);
-//             })
-//             .catch(err => done(err)); // closes User.create()
-//         })
-//         .catch(err => done(err)); // closes User.findOne()
-//     }
-//   )
-// );
-
-
+// USE passport.initialize() and passport.session() HERE:
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(flash());
+// default value for title local
+app.locals.title = 'Express API For React App - Generated with IronGenerator';
 
 
-app.use((req, res, next) => {
-  res.locals.user = req.user
-  res.locals.errorMessage = req.flash('error')
-  next();
-})
+// ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+}));
 
 
-
+// ROUTES MIDDLEWARE STARTS HERE:
 
 const index = require('./routes/index');
 app.use('/', index);
-
-
-app.use('/user', require('./routes/user-routes'))
-
-app.use('/post', require('./routes/post-routes'))
-
-app.use('/groups', require('./routes/group-routes'));
-
-app.use('/events', require('./routes/event-routes'))
+app.use('/api/auth', require('./routes/auth-routes'))
 
 
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 module.exports = app;
--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
@@ -10,7 +12,7 @@ cloudinary.config({
 
 var storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: 'UserImages', // The name of the folder in cloudinary
+  folder: process.env.CLOUDINARY_FOLDER, // The name of the folder in cloudinary
   allowedFormats: ['jpg', 'png'],
   filename: function (req, file, cb) {
     cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
