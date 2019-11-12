c3dacf2e4fcc2157eb9148cc1bec0987783b3915
Lauren Laskerr <llask@me.com>
2019-11-11 19:54:36 -0500

    new icons

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Signup from '../components/Auth/Signup'
 import Button from '../components/Button'
 
