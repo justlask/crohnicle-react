cf9189d8ea553808979cea060a14bac685cc24e9
Lauren Laskerr <llask@me.com>
2019-11-11 20:37:26 -0500

    a bit more mobile

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,23 +12,31 @@ export default class Home extends Component {
             <h1>Find other crohnies,<br></br> locally & online</h1>
             <Button className="join" name="join now" link="signup"/>
           </section>
-          <section className="flexcol">
-            <div className="boxholder">
-              <div className="box">
-              <img src="../images/chat.svg" alt=""/> 
+          <section className="container boxes">
+            <div className="row boxholder">
+              <div className="col-xs-12 col-md-6 col-lg-3 justify">
+                <div className="box">
+                <img src="../images/chat.svg" alt=""/> 
               <p>share your story</p>
+                </div>
               </div>
-              <div className="box">
+              <div className="col-xs-12 col-md-6 col-lg-3 justify">
+                <div className="box">
                 <img src="../images/hands.svg" alt=""/>
                 <p>connect with others</p>
+                </div>
               </div>
-              <div className="box">
+              <div className="col-xs-12 col-md-6 col-lg-3 justify">
+                <div className="box">
                 <img src="../images/people.svg" alt=""/>
                 <p>form groups</p>
+                </div>
               </div>
-              <div className="box">
-                <img src="../images/sports.svg" alt=""/>
+              <div className="col-xs-12 col-md-6 col-lg-3 justify">
+                  <div className="box">
+                  <img src="../images/sports.svg" alt=""/>
                 <p>create & host events</p>
+                  </div>
               </div>
             </div>
           </section>
