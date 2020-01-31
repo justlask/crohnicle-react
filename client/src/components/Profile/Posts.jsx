65a8685e4dd050bea5d12e0f39108b6d2712aaa8
Lauren Laskerr <llask@me.com>
2020-01-31 11:00:23 -0500

    styling for post card

--- client/src/components/Profile/Posts.jsx
+++ client/src/components/Profile/Posts.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import PostCard from './PostCard';
 
 const Posts = (props) => {
 
   const showPosts = () => {
     return props.posts.map((post, i) => {
       return (
-        <div className="postbox" key={i}>
-          <Link to={`/profile/${post.authorID.id}`} style={{color: '#4E1681'}}><p>{post.author}</p></Link>
-          <sub>{post.date}</sub>
-          <p>{post.title}</p>
-          <p style={{fontWeight: 400}}>{post.content}</p>
-          <img src={post.image} alt=""/>
-        </div>
+        <PostCard post={post} key={i} />
       )
     })
   }
