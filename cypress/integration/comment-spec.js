4d2b4fd5471513dfdedf5daaef20e61ad0adf7be
Lauren Laskerr <llask@me.com>
2020-02-08 18:30:42 -0500

    tests for comments

--- cypress/integration/comment-spec.js
+++ cypress/integration/comment-spec.js
@@ -12,15 +12,12 @@ context('Comments', () => {
   });
 
   it('will allow people to add a comment', () => {
+    cy.get(':nth-child(1) > .contentbox > :nth-child(4) > .interactbtns > :nth-child(2)')
+    cy.get(':nth-child(1) > .contentbox > :nth-child(4) > .interactbtns > :nth-child(2)').click()
+    cy.get('.addcomment > input[type="text"]').type('hello world123')
+    cy.get('.addcomment input[type="submit"]').click()
 
+    cy.contains('hello world123')
   });
-
-  it('will allow people to view comments', () => {
-    
-  })
-
-
-
-  
 
 });
